ce1c75568044bd5c4b4322c573880d276a1ffc36
Phil Wyett <philip.wyett@kathenas.org>
2021-07-29 09:43:34 +0100

    Check we have ffmpeg installed and available for use.

--- convert_audio_to_aac_stereo_128k.sh
+++ convert_audio_to_aac_stereo_128k.sh
@@ -7,6 +7,17 @@
 #
 
 #
+# Check we have ffmpeg installed and available for use.
+#
+if ! command -v ffmpeg &> /dev/null
+then
+    echo "ffmpeg could not be found."
+    echo "Please check you have ffmpeg installed on your system."
+    echo "Unable to continue. Exiting..."
+    exit
+fi
+
+#
 # Convert mp4 file audio to stereo (2 channel) aac.
 #
 for in_filename in *.mp4 *.MP4; do
--- mkv_to_quicktime_container_mp4.sh
+++ mkv_to_quicktime_container_mp4.sh
@@ -7,6 +7,17 @@
 #
 
 #
+# Check we have ffmpeg installed and available for use.
+#
+if ! command -v ffmpeg &> /dev/null
+then
+    echo "ffmpeg could not be found."
+    echo "Please check you have ffmpeg installed on your system."
+    echo "Unable to continue. Exiting..."
+    exit
+fi
+
+#
 # Convert matroska container video files to quicktime container mp4. 
 #
 for in_filename in *.mkv *.MKV; do
