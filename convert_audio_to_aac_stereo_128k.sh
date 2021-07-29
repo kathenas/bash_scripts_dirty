63a4f9c92ca1fe8a916c70f16a8328d447b9e59c
Phil Wyett <philip.wyett@kathenas.org>
2021-07-29 09:29:27 +0100

    Add author and license info.

--- convert_audio_to_aac_stereo_128k.sh
+++ convert_audio_to_aac_stereo_128k.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #
+# Author: Phil Wyett - philip.wyett@kathenas.org
+#
+# License: GPL3+ - See: license.txt enclosed in repository.
+#
+
+#
 # Convert mp4 file audio to stereo (2 channel) aac.
 #
 for in_filename in *.mp4 *.MP4; do
--- mkv_to_quicktime_container_mp4.sh
+++ mkv_to_quicktime_container_mp4.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #
+# Author: Phil Wyett - philip.wyett@kathenas.org
+#
+# License: GPL3+ - See: license.txt enclosed in repository.
+#
+
+#
 # Convert matroska container video files to quicktime container mp4. 
 #
 for in_filename in *.mkv *.MKV; do
