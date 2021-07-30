aefdbf11bab19e1fb800337a78d43d4681066524
Phil Wyett <philip.wyett@kathenas.org>
2021-07-30 14:02:56 +0100

    Add vp9 to vp9 script.

---
+++ gnome_screencast_vp9_to_vp9_colour_enhanced_2000k_bitrate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# Author: Phil Wyett - philip.wyett@kathenas.org
+#
+# License: GPL3+ - See: license.txt enclosed in repository.
+#
+
+#
+# Check we have ffmpeg installed and available for use.
+#
+if ! command -v ffmpeg &> /dev/null
+then
+    echo "ffmpeg could not be found."
+    echo "Please check you have ffmpeg installed on your system."
+    echo "Unable to continue. Exiting..."
+    exit
+fi
+
+#
+# Information.
+#
+# The gnome screencast recorder often produces video with poor colouring. The settings below can
+# be manipulated to produce a better final production webm file for future use, be that on your
+# website or social media etc.
+#
+
+#
+# Settings for the conversion. Edit as required.
+#
+set_gamma=1.0
+set_contrast=1.0
+set_brightness=0.0
+set_saturation=1.25
+
+#
+# Convert gnome internal screencast encoder vp9 webm file to vp9 with colour enhancement.
+#
+for in_filename in *.[wW][eE][bB][mM]
+do
+    ffmpeg -i "$in_filename" \
+        -vf eq=gamma="$set_gamma":contrast="$set_contrast":brightness="$set_brightness":saturation="$set_saturation" \
+        -codec:v libvpx-vp9 -b:v 2000k \
+        _"${in_filename%.*}.webm"
+    # Remove original input file.
+    rm -f "${in_filename%.*}.webm"
+    # Rename converted file to original filename.
+    mv -f "_${in_filename%.*}.webm" "${in_filename%.*}.webm"
+done
