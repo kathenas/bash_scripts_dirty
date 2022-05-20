8b16f6dd7b40206182f6dda0c1027d5d67c50859
Phil Wyett <philip.wyett@kathenas.org>
2022-05-20 15:39:33 +0100

    Add script for increasing volume/dB level in mp4.

---
+++ video/mp4_increase_volume.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#
+# Author: Phil Wyett - philip.wyett@kathenas.org
+#
+# License: GPL3+ - See: license.txt enclosed in repository.
+#
+
+#
+# Check we have ffmpeg installed and available for use.
+#
+if ! [ -x "$(command -v ffmpeg)" ]
+then
+    echo "ffmpeg could not be found."
+    echo "Please check you have ffmpeg installed on your system."
+    echo "Unable to continue. Exiting..."
+    exit
+fi
+
+#
+# Settings for the conversion. Edit as required.
+#
+set_increased_db_level=10dB
+
+#
+# Increase volume/dB level in mp4 file.
+#
+for in_filename in *.[mM][pP][4]
+do
+    ffmpeg -i "$in_filename" -vcodec copy -af "volume=$set_increased_db_level" "_${in_filename%.*}.mp4"
+    # Remove original input file.
+    rm -f "${in_filename%.*}.mp4"
+    # Rename converted file to original filename.
+    mv -f "_${in_filename%.*}.mp4" "${in_filename%.*}.mp4"
+done
